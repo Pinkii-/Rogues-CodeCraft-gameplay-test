58a1e99ca126f5b2fa2329b60cf11b44451367ac
Pinkii- <pinkiibiza@gmail.com>
2016-07-07 16:57:13 +0200

    Fix heroku

--- src/server.js
+++ src/server.js
@@ -1,5 +1,20 @@
-const {Game} = require('./Game.js')
+const express = require('express')
+const app = express()
+const http = require('http').Server(app)
+// const io = require('socket.io')(http)
+const {Game} = require('../src/Game.js')
+
+const PORT = process.env.PORT || 3000
 
 var game = new Game()
 
 console.log(game.turn)
+
+app.use(express.static('dist'))
+app.get('/', function (req, res) {
+  res.sendfile('../dist/index.html')
+})
+
+http.listen(PORT, function () {
+  console.log('listening on *:3000')
+})
